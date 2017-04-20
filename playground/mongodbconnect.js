c88033d6fa777288f0a3fab17b892b25eff982bf
onachassi <onachassi@gmail.com>
2017-04-19 22:57:27 -0700

    wrote document to database

---
+++ playground/mongodbconnect.js
@@ -0,0 +1,41 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj)
+
+// first argument url of db, could be amazon s3 or heroku ect.
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+	if(err){
+		return console.log('Unable to connect to MongoDB server')
+	}
+	console.log('Connected to MongoDB server.')
+
+	db.collection('Todos').insertOne({
+		text: 'Finish weddeliver app',
+		completed: false
+	},(err, result)=>{
+		if(err){
+			return console.log('Unable to insert todo', err)
+		}
+
+		console.log(JSON.stringify(result.ops, undefined, 2))
+	})
+
+	Doc into Users collection (name, age, location)
+
+	db.collection('Users').insertOne({
+		name: 'Ori',
+		age: 23,
+		location: 'Undisclosed Location'
+	},(err, result)=>{
+		if(err){
+			return console.log('Unable to insert todo', err)
+		}
+
+		console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2))
+	})
+
+
+	db.close()
+});
