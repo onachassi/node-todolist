b3bc75f05ee946f0129ecbeaf66007e45c80265c
onachassi <onachassi@gmail.com>
2017-04-20 23:38:13 -0700

    formatting

--- playground/mongodbconnect.js
+++ playground/mongodbconnect.js
@@ -9,7 +9,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 	if(err){
 		return console.log('Unable to connect to MongoDB server')
 	}
-	console.log('Connected to MongoDB server.')
+	console.log('-Connected to MongoDB Server-')
 
 	db.collection('Todos').insertOne({
 		text: 'Finish weddeliver app',
