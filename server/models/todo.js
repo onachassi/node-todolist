0c8399d2bf7519b59c6f94f80699ad390ad81d71
onachassi <onachassi@gmail.com>
2017-04-21 17:15:48 -0700

    added server commands for post

---
+++ server/models/todo.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo',{
+	// validators
+	text: {
+		type: String,
+		required: true,
+		minlength: 1,
+		trim: true
+	}, 
+	completed: {
+		type: Boolean,
+		default: false
+	},
+	completedAt: {
+		type: Number,
+		default: null
+	}
+});
+
+module.exports = {
+	Todo
+}
